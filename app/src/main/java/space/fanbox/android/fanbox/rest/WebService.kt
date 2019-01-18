e0746b961306b92ce34f2ca7930914869e11cec6
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-18 13:44:25 +0100

    Initial Commit
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

---
+++ app/src/main/java/space/fanbox/android/fanbox/rest/WebService.kt
@@ -0,0 +1,11 @@
+package space.fanbox.android.fanbox.rest
+
+import io.reactivex.Observable
+import retrofit2.http.GET
+import space.fanbox.android.fanbox.model.Letter
+
+interface WebService {
+
+    @GET("/letters/")
+    fun getLetters(): Observable<List<Letter>>
+}
