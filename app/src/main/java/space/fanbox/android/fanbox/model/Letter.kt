914a06188be0f7c21acd79896569aad43b798d3a
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-18 13:14:03 +0100

    Initial Commit
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

---
+++ app/src/main/java/space/fanbox/android/fanbox/model/Letter.kt
@@ -0,0 +1,19 @@
+package space.fanbox.android.fanbox.model
+
+data class Letter(
+    val id: String,
+    val subject: String,
+    val body: String,
+    val attachment: String,
+    val closing: String,
+    val recipient: String,
+    val slug: String,
+    val sender_name: String,
+    val sender_id: String,
+    val stamp_id: String,
+    val category: List<String>,
+    val love: String,
+    val hate: String,
+    val views: String,
+    val date_created: String
+)
