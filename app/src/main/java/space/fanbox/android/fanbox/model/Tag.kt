b77f180b825b6e2b46b67d222fc21e0305f996a3
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-22 21:28:04 +0100

    Initial Commit
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

---
+++ app/src/main/java/space/fanbox/android/fanbox/model/Tag.kt
@@ -0,0 +1,9 @@
+package space.fanbox.android.fanbox.model
+
+data class Tag (
+    val id: String,
+    val label: String,
+    val slug: String,
+    val color: String,
+    val text_color: String
+)
