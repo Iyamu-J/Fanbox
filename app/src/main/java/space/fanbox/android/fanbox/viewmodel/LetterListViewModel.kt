aaa31301a28e0cc102be4f53e6c151d9a583e464
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-02-12 12:25:22 +0100

    Major code changes
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

--- app/src/main/java/space/fanbox/android/fanbox/viewmodel/LetterListViewModel.kt
+++ app/src/main/java/space/fanbox/android/fanbox/viewmodel/LetterListViewModel.kt
@@ -3,8 +3,10 @@ package space.fanbox.android.fanbox.viewmodel
 import android.util.Log
 import android.view.View
 import androidx.lifecycle.MutableLiveData
+import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import space.fanbox.android.fanbox.R
@@ -13,6 +15,8 @@ import space.fanbox.android.fanbox.di.BaseViewModel
 import space.fanbox.android.fanbox.model.Letter
 import space.fanbox.android.fanbox.rest.WebService
 import space.fanbox.android.fanbox.ui.LetterListAdapter
+import space.fanbox.android.fanbox.utils.RefreshInterval
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class LetterListViewModel(private val letterDao: LetterDao) : BaseViewModel() {
@@ -21,6 +25,9 @@ class LetterListViewModel(private val letterDao: LetterDao) : BaseViewModel() {
     lateinit var webService: WebService
 
     private lateinit var subscription: Disposable
+    private var compositeDisposable: CompositeDisposable
+
+    private val refreshInterval: RefreshInterval<String>
 
     val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
     val errorMessage: MutableLiveData<Int> = MutableLiveData()
@@ -32,32 +39,55 @@ class LetterListViewModel(private val letterDao: LetterDao) : BaseViewModel() {
 
     init {
         loadLetters()
+        compositeDisposable = CompositeDisposable()
+        refreshInterval = RefreshInterval(30, TimeUnit.MINUTES)
     }
 
     override fun onCleared() {
         super.onCleared()
         subscription.dispose()
+        compositeDisposable.clear()
     }
 
     private fun loadLetters() {
         subscription = Observable.fromCallable { letterDao.all }
             .concatMap { dbLetterList ->
-                if (dbLetterList.isEmpty())
+                if (dbLetterList.isEmpty()) {
                     webService.getLetters().concatMap { apiLetterList ->
                         letterDao.insertAllLetters(*apiLetterList.toTypedArray())
                         Observable.just(apiLetterList)
                     }
-                else
+                } else {
+                    if (refreshInterval.shouldFetch("letters")) {
+                        refreshLetters()
+                    }
                     Observable.just(dbLetterList)
+                }
             }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .doOnSubscribe { onRetrieveLetterListStart() }
             .doOnTerminate { onRetrieveLetterListFinish() }
+            .doAfterNext {
+
+            }
             .subscribe(
                 { result -> onRetrieveLetterListSuccess(result) },
-                { error -> onRetrieveLetterListError(error) }
+                { error ->
+                    onRetrieveLetterListError(error)
+                    refreshInterval.reset("letters")
+                }
             )
+    }
+
+    private fun refreshLetters() {
+
+        compositeDisposable.add(
+            webService.getLetters()
+                .subscribeOn(Schedulers.io())
+                .subscribe { result -> updateLocalSource(result) }
+        )
+        Log.i(LetterListViewModel::class.java.simpleName, "New Updates")
     }
 
     private fun onRetrieveLetterListStart() {
@@ -72,11 +102,21 @@ class LetterListViewModel(private val letterDao: LetterDao) : BaseViewModel() {
     private fun onRetrieveLetterListSuccess(letterList: List<Letter>) {
         letterListAdapter.setLetterList(letterList)
         errorMessage.value = null
-//        Log.i(LetterListViewModel::class.java.simpleName, letterList.toString())
     }
 
     private fun onRetrieveLetterListError(error: Throwable) {
         errorMessage.value = R.string.error_message
         Log.i(LetterListViewModel::class.java.simpleName, error.message)
+    }
+
+    private fun updateLocalSource(letterList: List<Letter>) {
+        Completable
+            .fromAction {
+                letterDao.insertAllLetters(*letterList.toTypedArray())
+            }
+            .subscribeOn(Schedulers.io())
+            .subscribe()
+
+        Log.i(LetterListViewModel::class.java.simpleName, "Updates Added to DB")
     }
 }
