634c5b7bf17477ecf384f5c1ab5b2c0d3384ea08
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-02-13 13:06:06 +0100

    Get singleton database instance
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

--- app/src/main/java/space/fanbox/android/fanbox/viewmodel/LetterViewModelFactory.kt
+++ app/src/main/java/space/fanbox/android/fanbox/viewmodel/LetterViewModelFactory.kt
@@ -1,22 +1,16 @@
 package space.fanbox.android.fanbox.viewmodel
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.room.Room
-import space.fanbox.android.fanbox.database.AppDatabase
+import space.fanbox.android.fanbox.database.LettersDatabase
 
-class LetterViewModelFactory(private val activity: AppCompatActivity) : ViewModelProvider.Factory {
+class LetterViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(LetterListViewModel::class.java)) {
-            val db = Room.databaseBuilder(
-                activity.applicationContext,
-                AppDatabase::class.java,
-                "letters"
-            )
-                .build()
+            val db = LettersDatabase.getInstance(context)
 
             return LetterListViewModel(db.letterDao()) as T
         }
