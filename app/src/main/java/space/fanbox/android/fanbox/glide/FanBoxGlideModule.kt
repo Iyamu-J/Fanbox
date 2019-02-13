4f939bc687dd22218fa99cdec2df4bc6b7428b5e
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-02-13 23:03:35 +0100

    Initial Commit
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

---
+++ app/src/main/java/space/fanbox/android/fanbox/glide/FanBoxGlideModule.kt
@@ -0,0 +1,7 @@
+package space.fanbox.android.fanbox.glide
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class FanBoxGlideModule : AppGlideModule() 
